ef95d07bdd8571d2e9bf8c4d6006674b4f3b0280
iamThaslim <thaslim313@gmail.com>
2024-06-26 17:08:18 +0530

    navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <>
     <h1 className="text-2xl">Dashboard</h1>
-    <Button variant='destructive' size='lg' className="text-blue-400">Click me</Button>
     </>
   );
 }
